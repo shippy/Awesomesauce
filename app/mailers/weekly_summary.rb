869938f306ffa94f9648b26fe9587191325b6e16
Simon Podhajsky <simon.podhajsky@gmail.com>
2013-02-24 15:36:40 -0500

    Outgoing mailers

--- app/mailers/weekly_summary.rb
+++ app/mailers/weekly_summary.rb
@@ -1,3 +1,8 @@
 class WeeklySummary < ActionMailer::Base
   default from: "aweoftoday@gmail.com"
+  
+  def summary(user)
+    @thoughts = Thought.where(:user_id => user.id).last(5)
+    mail(:to => user.email, :subject => "All the good things in your life!")
+  end
 end
