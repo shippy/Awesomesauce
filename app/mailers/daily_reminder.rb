8ae48f60d28e1b2403fce30a4a4cc9d6bc069c73
Simon Podhajsky <simon.podhajsky@gmail.com>
2013-02-24 15:36:40 -0500

    Outgoing mailers

--- app/mailers/daily_reminder.rb
+++ app/mailers/daily_reminder.rb
@@ -1,5 +1,6 @@
 class DailyReminder < ActionMailer::Base
   default from: "aweoftoday@gmail.com"
+  default reply_to: "688bc4f4e879c7e9f137@cloudmailin.net"
   
   def query(user)
     mail(:to => user.email, :subject => "What are you grateful for today?")
