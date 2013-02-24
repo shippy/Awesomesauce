6c963f7ddf9890ec502aaacab0cbef8ce8f58b15
Simon Podhajsky <simon.podhajsky@gmail.com>
2013-02-24 04:06:26 -0500

    Rewrote to use devise, unconnected posts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Myapp::Application.routes.draw do
+  devise_for :users
+
+  get "home/index"
+
+  resources :posts
+  
+  root :to => "home#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
