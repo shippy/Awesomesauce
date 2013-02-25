12fccb731c1351bf4a4968a6f3cc5da520341069
Simon Podhajsky <simon.podhajsky@gmail.com>
2013-02-25 15:27:03 -0500

    Set up e-mail sending for dev and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Myapp::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "myapp.herokuapp.com",
+    domain: "awesomesauce1.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
