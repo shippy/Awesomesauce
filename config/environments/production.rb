c56fd0a6acc1862431de40d65d15a26dbc176d02
Simon Podhajsky <simon.podhajsky@gmail.com>
2013-02-25 18:18:37 -0500

    Old-style assignments for Heroku deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,13 +72,13 @@ Myapp::Application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "awesomesauce1.herokuapp.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    openssl_verify_mode: 'none'
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    :address => "smtp.gmail.com",
+    :port => 587,
+    # domain => "awesomesauce1.herokuapp.com",
+    :authentication => "plain",
+    :enable_starttls_auto => true,
+    :openssl_verify_mode => 'none'
+    :user_name => ENV["GMAIL_USERNAME"],
+    :password => ENV["GMAIL_PASSWORD"]
   }
 end
