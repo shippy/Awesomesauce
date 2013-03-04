26134fc05d98ac2e05e7b7994215f799dc2c51cf
Simon Podhajsky <simon.podhajsky@gmail.com>
2013-03-03 22:13:34 -0500

    Number of ATD in summary increased to 7

--- app/mailers/weekly_summary.rb
+++ app/mailers/weekly_summary.rb
@@ -2,7 +2,7 @@ class WeeklySummary < ActionMailer::Base
   default from: "aweoftoday@gmail.com"
   
   def summary(user)
-    @thoughts = Thought.where(:user_id => user.id).last(5)
+    @thoughts = Thought.where(:user_id => user.id).last(7)
     @cute = Cute.random
     mail(:to => user.email, :subject => "All the good things in your life!")
   end
